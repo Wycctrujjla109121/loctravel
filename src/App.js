81a272a150a86e9734e835b2254636ce54b82c23
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2022-05-18 17:12:20 +0300

    last fix modal window

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       {/* <EditProfile /> */}
-      {/* <Message /> */}
+      <Message />
       {/* <Reviews /> */}
       {/* <RatePlace /> */}
       {/* <Visit /> */}
--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -6,7 +6,7 @@ import s from './Message.module.scss'
 const Message = () => {
     return (
         <form className={s.message}>
-            <h2 className={s.message__title}>Напишите сообщение пользователю <span className={s.message__name}>Евгений Андреев</span></h2>
+            <h2 className={s.message__title}>Напишите сообщение <span className={s.message__name}>Евгений Андреев</span></h2>
             <button className={s.message__close}></button>
             <textarea className={s.message__textarea} name="" id="" cols="30" rows="10" placeholder='Сообщение'></textarea>
             <div className={s.message__button}>
